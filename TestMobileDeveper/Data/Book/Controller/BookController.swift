6c25ba9663bba0a0dbaa11a66883b45cb3e0e6aa
Keen <zhekeen90@gmail.com>
2019-11-17 22:09:03 +0700

    feature/Controller update 1

--- TestMobileDeveper/Data/Book/Controller/BookController.swift
+++ TestMobileDeveper/Data/Book/Controller/BookController.swift
@@ -34,7 +34,7 @@ class BookController: UIViewController {
     }
     
     func setNavigation() {
-        navigationItem.title = "My Book"
+        navigationItem.title = "Book"
         
         view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
